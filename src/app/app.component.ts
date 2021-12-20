2d72e759e84883999b645b8a8e8dfc59c1ac5f71
Jenny <evgeniia.vader@gmail.com>
2021-12-20 14:35:52 +0200

    made simple cards with Angular and TypeScript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+
+posts = [
+  {
+    title: 'Neat Tree',
+    imageUrl: 'assets/images/tree.jpeg',
+    username: 'nature',
+    content: 'bebebe'
+  },
+  {
+    title: 'Snowy Mountain',
+    imageUrl: 'assets/images/mountain.jpeg',
+    username: 'mountainlover',
+    content: 'bebebe'
+  },
+  {
+    title: 'Mountain Biking',
+    imageUrl: 'assets/images/biking.jpeg',
+    username: 'biking',
+    content: 'bebebe'
+  }
+]
+
 }
